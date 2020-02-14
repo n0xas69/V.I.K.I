475ceb427bb464cd6efc9b1f4940354e177128c5
neiwak <romain.dusson@gmail.com>
2020-02-14 14:18:56 +0100

    add main

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+import speech_recognition as sr
+import os
+
+def command():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Prêt")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+    try:
+        command = r.recognize_google(audio).lower()
+        print("Commande : " + command + '\n')
+
+    # boucle si la commande passé n'est pas reconnu
+    except sr.UnknownValueError:
+        print("Commande inconnu")
+        command = myCommand()
+
+    return command
+
+def assistant():
+    ''' Condition suivant ce que l'utilisateur dit '''
+    if "affiche salut" in command:
+        print("Salut")
+
+    elif "affiche sarce" in command:
+        print("sarce")
+
+
 
